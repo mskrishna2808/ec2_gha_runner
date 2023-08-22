f2afab77fcc5235fd2bb5c6dd4959be4abde5439
Mendu Sai krishna <mskrishna2808@gmail.com>
2023-08-22 17:09:12 +0530

    Updated labels for GHA

--- terraform.tfvars
+++ terraform.tfvars
@@ -4,4 +4,4 @@ iam_instance_role = "IamInstanceRole"
 vpc_zone_identifier = ["subnet-0829fbc8bbc0f3b34"]
 ami_owner           = ["amazon"]
 user_data           = ["user_data_1.sh", "user_data_1.sh"]
-runner_name         = ["pcp", "pcpo-ui"]
+runner_name         = ["runner-1"]
--- user_data_1.sh
+++ user_data_1.sh
@@ -13,7 +13,7 @@ aws secretsmanager get-secret-value --secret-id ec2_user_data --query SecretStri
 mkdir actions-runner && cd actions-runner && \
 curl -o actions-runner-linux-x64-2.308.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.308.0/actions-runner-linux-x64-2.308.0.tar.gz && \
 tar xzf ./actions-runner-linux-x64-2.308.0.tar.gz && \
-./config.sh --unattended --url https://github.com/mskrishna2808/ec2_gha_runner --token $(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $(aws secretsmanager get-secret-value --secret-id github_token --query SecretString --output text --region ap-south-1)" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/mskrishna2808/ec2_gha_runner/actions/runners/registration-token | jq .token | tr -d '"') && \
+./config.sh --unattended --url https://github.com/mskrishna2808/ec2_gha_runner --token $(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $(aws secretsmanager get-secret-value --secret-id github_token --query SecretString --output text --region ap-south-1)" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/mskrishna2808/ec2_gha_runner/actions/runners/registration-token | jq .token | tr -d '"') --labels self-hosted-runner,new-runner && \
 sudo ./svc.sh install && \
 sudo ./svc.sh start"
 #======================================================================================
